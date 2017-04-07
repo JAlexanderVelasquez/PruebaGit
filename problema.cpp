4c2e11404289e7ea6eee71479debdecf569eff33
Nicolas <nicolas.velasquez@correounivalle.edu.co>
2017-04-07 14:45:46 -0500

    hola xd

---
+++ problema.cpp
@@ -0,0 +1,30 @@
+/*
+ * Descripcion: Problema4
+ * Nombre: Andres Mosquera, Nicolas Velasquez, Alexander Velasquez
+ * Fecha de Creacion: 1-03-2017
+ * Fecha de Ult de modificacion: 07-04-2017
+ * 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+using namespace std;
+
+int main(){
+	
+		string nombre;
+		string edad;
+		string estado;
+		string comando;
+		
+	
+	
+		
+
+}
+
+				
